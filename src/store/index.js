081340d9d815d99f3c283b324bc51744e7c0f8f3
voidrlm <varjun609@gmail.com>
2022-11-17 03:41:44 +0530

    Added store index.js

---
+++ src/store/index.js
@@ -0,0 +1,29 @@
+import Vue from "vue";
+import Vuex from "vuex";
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    currentUser: {},
+  },
+  mutations: {
+    setCurrentUser(state, currentUser) {
+      return (state.currentUser = currentUser);
+    },
+  },
+  actions: {
+    setCurrentUser(context, currentUser) {
+      if (context.currentUser === currentUser) return;
+      context.commit("setCurrentUser", currentUser);
+    },
+
+    resetState(context) {
+      context.dispatch("setCurrentUser", {});
+    },
+  },
+  getters: {
+    currentUser: (state) => state.currentUser,
+  },
+});
+
+export default store;
