1003005f6b809af82499d47c1ff39ded003fba69
voidrlm <varjun609@gmail.com>
2022-11-18 00:37:47 +0530

    Added router guards

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import routes from "./routes";
+import store from "@/store/index";
 Vue.use(VueRouter);
 
 const router = new VueRouter({
@@ -8,6 +9,26 @@ const router = new VueRouter({
 });
 
 export default router;
+
+router.beforeEach((to, from, next) => {
+  let isUnauthorized =
+    JSON.stringify(store.getters.currentUser) === JSON.stringify({});
+  if (to.matched.some((record) => record.meta.authorized)) {
+    if (isUnauthorized) {
+      next({
+        path: "/",
+      });
+    } else {
+      if (to.matched.some((record) => record.meta.authorized)) {
+        if (!isUnauthorized) {
+          next();
+        } else next({ path: from.path });
+      }
+    }
+  } else {
+    next();
+  }
+});
 router.afterEach((to) => {
   document.title = to.name !== null ? to.name : "Vue And Vuetify";
 });
