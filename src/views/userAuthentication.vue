e01c107f99feb8d3870c7e4eebd2b573b995ac78
voidrlm <varjun609@gmail.com>
2022-11-17 13:34:23 +0530

    Login page design changes

--- src/views/userAuthentication.vue
+++ src/views/userAuthentication.vue
@@ -1,28 +1,51 @@
 <template>
-  <v-container fluid fill-height>
+  <v-container fluid fill-height style="background: black">
     <v-layout align-center justify-center>
       <v-flex xs12 sm8 md4>
-        <v-card class="elevation-12">
-          <v-toolbar color="accent">
-            <v-toolbar-title>Login</v-toolbar-title>
-          </v-toolbar>
-          <v-card-text>
-            <v-form>
-              <v-text-field
-                prepend-icon="mdi-account"
-                label="UserName/Mail"
-                type="text"
-              ></v-text-field>
-              <v-text-field
-                prepend-icon="mdi-lock"
-                label="Password"
-                type="password"
-              ></v-text-field>
-            </v-form>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn color="primary" to="/">Login</v-btn>
+        <v-card class="rounded-xl" dark>
+          <v-card-title class="justify-center pa-5">Login</v-card-title>
+
+          <v-form class="px-8 mb-n2">
+            <v-text-field
+              color="black"
+              prepend-inner-icon="mdi-account"
+              label="UserName/Mail"
+              type="text"
+              solo-inverted
+              rounded
+            ></v-text-field>
+            <v-text-field
+              color="black"
+              prepend-inner-icon="mdi-lock"
+              label="Password"
+              type="password"
+              solo-inverted
+              rounded
+            ></v-text-field>
+          </v-form>
+
+          <v-card-actions class="justify-center nt-n5 pb-5">
+            <v-btn
+              rounded
+              color="accent"
+              @click.stop="performAction('signup')"
+              dark
+              elevation="0"
+              class="mr-5 px-4"
+              :class="$vuetify.theme.dark ? 'white--text' : 'black--text'"
+            >
+              Sign Up
+            </v-btn>
+            <v-btn
+              rounded
+              color="accent"
+              @click.stop="performAction('login')"
+              dark
+              elevation="0 px-4"
+              :class="$vuetify.theme.dark ? 'white--text' : 'black--text'"
+            >
+              Login
+            </v-btn>
           </v-card-actions>
         </v-card>
       </v-flex>
@@ -34,5 +57,10 @@ export default {
   name: "user-auth-component",
   data: () => ({}),
   computed: {},
+  methods: {
+    performAction(action) {
+      console.log(action);
+    },
+  },
 };
 </script>
