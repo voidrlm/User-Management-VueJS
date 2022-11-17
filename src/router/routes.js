e26610698aac95cbd566ad4add4d2d713bfd17a3
voidrlm <varjun609@gmail.com>
2022-11-18 00:37:15 +0530

    Addedf meta in routes.js

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,16 +2,19 @@ const routes = [
   {
     path: "/",
     name: "Login",
+    meta: { authorized: false },
     component: () => import("../views/userAuthentication.vue"),
   },
   {
     path: "/dashboard",
     name: "Dashboard",
+    meta: { authorized: true },
     component: () => import("../views/dashboard.vue"),
   },
   {
     path: "/calendar",
     name: "Calendar",
+    meta: { authorized: true },
     component: () => import("../views/calendar.vue"),
   },
 ];
