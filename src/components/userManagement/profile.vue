68e2fe50429b07b66e6e687fdd3cc316646fa312
voidrlm <varjun609@gmail.com>
2022-11-18 15:11:44 +0530

    Added profile view component

---
+++ src/components/userManagement/profile.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-dialog
+    :value="showProfile"
+    persistent
+    max-width="400"
+    content-class="rounded-xl"
+    :overlay-opacity="0.8"
+  >
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: "profile-component",
+  props: { showProfile: Boolean },
+  data: () => ({}),
+  computed: {},
+  methods: {},
+};
+</script>
