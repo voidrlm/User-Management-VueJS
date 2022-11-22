210bb1b1d407dfc89b262da5a64c00b7f57b176c
voidrlm <varjun609@gmail.com>
2022-11-23 02:36:09 +0530

    Added vue cookie support in main.js

--- src/main.js
+++ src/main.js
@@ -3,9 +3,10 @@ import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
 import router from "./router";
 import store from "./store";
+import VueCookies from "vue-cookies";
 
 Vue.config.productionTip = false;
-
+Vue.use(VueCookies);
 new Vue({
   vuetify,
   router,
